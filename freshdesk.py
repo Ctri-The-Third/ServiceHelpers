886f489d9fd2046d40f85813a9fd895dc1309213
C'tri <CtriCoding@Ctri.co.uk>
2022-04-01 13:07:10 +0100

    Added "created" and "updated" fields to Freshdesk and Jira ticket classes.

--- freshdesk.py
+++ freshdesk.py
@@ -1,47 +1,50 @@
 import json
-import requests
-import datetime
-import re
-import math
 import logging
+import math
+import re
+from datetime import datetime
 
+import requests
 
-#get all open FD tickets
-#get all open trello cards
+# get all open FD tickets
+# get all open trello cards
 
-#cross reference all Trello Cards with a custom field to their FD ticket brethren
-#turn unaffilliated tickets into cards.
-
+# cross reference all Trello Cards with a custom field to their FD ticket brethren
+# turn unaffilliated tickets into cards.
 
 
 lo = logging.getLogger("freshdesk")
-class FreshDesk():
+TIMESTAMP_FORMAT = r"%Y-%m-%dT%H:%M:%SZ"
+
+
+class FreshDesk:
     def __init__(self, host, api_key) -> None:
         self.host = host
         self.api_key = api_key
         pass
-    def search_fd_tickets(self,  query_string):
+
+    def search_fd_tickets(self, query_string):
         "now paginated!"
-        
-        #sample query string = ((agent_id:%s OR agent_id:null) AND (status:2 OR status:3))
-        ticketObj = {} 
+
+        # sample query string = ((agent_id:%s OR agent_id:null) AND (status:2 OR status:3))
+        ticketObj = {}
         current_page = 1
         get_next_page = True
         last_content = {}
         while get_next_page:
             url = f'https://{self.host}/api/v2/search/tickets?page={current_page}&query="{query_string}"'
-            r = requests.get (
-                url,
-                headers = self._get_default_headers()
-            )
-        
+            r = requests.get(url, headers=self._get_default_headers())
+
             if r.status_code != 200:
-                lo.warning("Unexpected response code [%s], whilst getting ticket list - reason:\t%s" % (r.status_code,r.content))
+                lo.warning(
+                    "Unexpected response code [%s], whilst getting ticket list - reason:\t%s"
+                    % (r.status_code, r.content)
+                )
                 return {}
-            
+
             if r.content != last_content:
                 tickets = json.loads(r.content)
-                if "results" in tickets: 
+                if "results" in tickets:
                     for ticket in tickets["results"]:
                         new_ticket = FreshdeskTicket()
                         new_ticket.from_dict(ticket)
@@ -49,39 +52,40 @@ class FreshDesk():
                 last_content = r.content
             else:
                 get_next_page = False
-                current_page += 1 
-            
-        
+                current_page += 1
+
         return ticketObj
 
     def _get_default_headers(self) -> dict:
         "headers with auth token for requests"
         AuthString = "Basic %s" % (self.api_key)
-        return {'Authorization':AuthString,
-            'Content-Type':'application/json'}
+        return {"Authorization": AuthString, "Content-Type": "application/json"}
 
-    def fetch_worklogs(self,ticketID:str):
+    def fetch_worklogs(self, ticketID: str):
         returnObj = []
         getNextPage = True
         oldResponse = ""
         currentPage = 0
-        
+
         worklogs = {}
         while getNextPage == True:
             currentPage = currentPage + 1
-            url = 'https://%s/api/v2/tickets/%s/time_entries' % (self.host,ticketID)            
-            r = requests.get ( url, headers = self._get_default_headers() )
-        
+            url = "https://%s/api/v2/tickets/%s/time_entries" % (self.host, ticketID)
+            r = requests.get(url, headers=self._get_default_headers())
+
             if r.status_code != 200:
-                lo.warn("Unexpected response code [%s], whilst getting worklogs for %s" % (r.status_code,ticketID))
+                lo.warn(
+                    "Unexpected response code [%s], whilst getting worklogs for %s"
+                    % (r.status_code, ticketID)
+                )
                 print(r.content)
-                return 
+                return
             if r.content == oldResponse:
                 getNextPage = False
-            else :
+            else:
                 oldResponse = r.content
-            
-            try: 
+
+            try:
                 response = json.loads(r.content)
             except Exception as e:
                 lo.error("Couldn't parse json of worklogs")
@@ -90,12 +94,12 @@ class FreshDesk():
             for worklog in response:
                 if "id" in worklog:
                     worklogs[worklog["id"]] = worklog
-                    
+
         if len(returnObj) > 0:
             lo.debug(worklogs)
         return list(worklogs.values())
 
-    def fetch_comments(self,ticket:str) -> list:
+    def fetch_comments(self, ticket: str) -> list:
 
         #'https://domain.freshdesk.com/api/v2/tickets/1/conversations?page=2'
         getNextPage = True
@@ -105,42 +109,45 @@ class FreshDesk():
         comments = []
         while getNextPage == True:
             currentPage = currentPage + 1
-            url = 'https://%s/api/v2/tickets/%s/conversations?page=%s' % (self.host,ticket,currentPage)
-            
+            url = "https://%s/api/v2/tickets/%s/conversations?page=%s" % (
+                self.host,
+                ticket,
+                currentPage,
+            )
+
             try:
-                r = requests.get (
-                    url,
-                    headers = self._get_default_headers()
-                )
+                r = requests.get(url, headers=self._get_default_headers())
                 conversation = json.loads(r.content)
                 if len(conversation) < 30:
                     getNextPage = False
             except Exception as e:
                 lo.error("unrecoverable error when getting comments, %s", e)
-                return [] 
-                
-            
+                return []
+
             if r.status_code != 200:
-                print("Unexpected response code [%s], whilst getting ticket list" % r.status_code)
+                print(
+                    "Unexpected response code [%s], whilst getting ticket list"
+                    % r.status_code
+                )
                 print(r.content)
-                return 
+                return
             if r.content == oldResponse:
                 getNextPage = False
-            else :
+            else:
                 oldResponse = r.content
                 for message in conversation:
                     comments.append(message)
         if len(comments) > 0:
-            logging.debug("[%s]" % countOfMessages)    
+            logging.debug("[%s]" % countOfMessages)
         return comments
 
-    def get_fd_tickets_updated_on(self,targetdate:str):
+    def get_fd_tickets_updated_on(self, targetdate: str):
         "expects a string in the format %Y-%m-%d"
-        return self.search_fd_tickets(f"updated_at:\'{targetdate}\'")
-        
-    def get_worklogs(self,ticketID):
-        #/api/v2/tickets/[id]/time_entries
-        #returns all tickets associated with me, or unassigned
+        return self.search_fd_tickets(f"updated_at:'{targetdate}'")
+
+    def get_worklogs(self, ticketID):
+        # /api/v2/tickets/[id]/time_entries
+        # returns all tickets associated with me, or unassigned
 
         returnObj = []
         getNextPage = True
@@ -149,67 +156,76 @@ class FreshDesk():
         while getNextPage == True:
             currentPage = currentPage + 1
 
-
-            url = 'https://%s/api/v2/tickets/%s/time_entries' % (self.host,ticketID)
-            AuthString = "Basic %s" % (self.api_key) 
-            r = requests.get (
+            url = "https://%s/api/v2/tickets/%s/time_entries" % (self.host, ticketID)
+            AuthString = "Basic %s" % (self.api_key)
+            r = requests.get(
                 url,
-                headers = {'Authorization':AuthString,
-                'Content-Type':'application/json'}
-                )
-        
-            if r.status_code != 200:
-                logging.warn("Unexpected response code [%s], whilst getting worklogs for %s" % (r.status_code,ticketID))
-                print(r.content)
-                return 
+                headers={
+                    "Authorization": AuthString,
+                    "Content-Type": "application/json",
+                },
+            )
 
+            if r.status_code != 200:
+                logging.warn(
+                    "Unexpected response code [%s], whilst getting worklogs for %s"
+                    % (r.status_code, ticketID)
+                )
+                print(r.content)
+                return
 
             if r.content == oldResponse:
                 getNextPage = False
-            else :
+            else:
                 oldResponse = r.content
 
-            
             response = json.loads(r.content)
-            
+
             for worklog in response:
                 returnObj.append(worklog)
-                    
-        
-        
+
         return returnObj
 
-    def get_comments(self,ticket):
+    def get_comments(self, ticket):
         #'https://domain.freshdesk.com/api/v2/tickets/1/conversations?page=2'
         getNextPage = True
         oldResponse = ""
-        
+
         currentPage = 0
-        messages = [] 
+        messages = []
         while getNextPage == True:
             currentPage = currentPage + 1
-            url = 'https://%s/api/v2/tickets/%s/conversations?page=%s' % (cfg.FreshdeskURL,ticket,currentPage)
+            url = "https://%s/api/v2/tickets/%s/conversations?page=%s" % (
+                cfg.FreshdeskURL,
+                ticket,
+                currentPage,
+            )
             AuthString = "Basic %s" % (self.api_key)
             try:
-                r = requests.get (
+                r = requests.get(
                     url,
-                    headers = {'Authorization':AuthString,
-                    'Content-Type':'application/json'}
+                    headers={
+                        "Authorization": AuthString,
+                        "Content-Type": "application/json",
+                    },
                 )
                 conversation = json.loads(r.content)
                 if len(conversation) < 30:
                     getNextPage = False
             except Exception as e:
                 return []
-            
+
             if r.status_code != 200:
-                print("Unexpected response code [%s], whilst getting ticket list" % r.status_code)
+                print(
+                    "Unexpected response code [%s], whilst getting ticket list"
+                    % r.status_code
+                )
                 print(r.content)
-                return 
+                return
             if r.content == oldResponse:
                 getNextPage = False
-                
-            else :
+
+            else:
                 oldResponse = r.content
                 maybeJSON = r.content
                 for message in conversation:
@@ -217,11 +233,7 @@ class FreshDesk():
         return messages
 
 
-
-
-
-
-class FreshdeskTicket():
+class FreshdeskTicket:
     def __init__(self) -> None:
         self.status = ""
         self.id = 0
@@ -231,16 +243,46 @@ class FreshdeskTicket():
         self.description = ""
         self.type = ""
         self.custom_fields = {}
+        self.created = datetime.min
+        self.updated = datetime.min
+        self.response_group = 0
 
-
-    def from_dict(self,new_params:dict) -> None:
+    def from_dict(self, new_params: dict) -> None:
         self.status = new_params["status"] if "status" in new_params else self.status
         self.id = new_params["id"] if "id" in new_params else self.id
-        self.responder_id = new_params["responder_id"] if "responder_id" in new_params else self.responder_id
-        self.priority = new_params["priority"] if "priority" in new_params else self.priority
-        self.subject = new_params["subject"] if "subject" in new_params else self.subject
-        self.description = new_params["description_text"] if "description_text" in new_params else self.description
+        self.responder_id = (
+            new_params["responder_id"]
+            if "responder_id" in new_params
+            else self.responder_id
+        )
+        self.priority = (
+            new_params["priority"] if "priority" in new_params else self.priority
+        )
+        self.subject = (
+            new_params["subject"] if "subject" in new_params else self.subject
+        )
+        self.description = (
+            new_params["description_text"]
+            if "description_text" in new_params
+            else self.description
+        )
         self.type = new_params["type"] if "type" in new_params else self.type
-        self.custom_fields = new_params["custom_fields"] if "custom_fields" in new_params else self.custom_fields
+        self.custom_fields = (
+            new_params["custom_fields"]
+            if "custom_fields" in new_params
+            else self.custom_fields
+        )
+        self.created = (
+            datetime.strptime(new_params["created_at"], TIMESTAMP_FORMAT)
+            if "created_at" in new_params
+            else self.created
+        )
+        self.updated = (
+            datetime.strptime(new_params["updated_at"], TIMESTAMP_FORMAT)
+            if "updated_at" in new_params
+            else self.updated
+        )
 
-        
+        self.response_group = (
+            new_params["group_id"] if "group_id" in new_params else self.response_group
+        )
--- jira.py
+++ jira.py
@@ -1,29 +1,39 @@
+import datetime
 import json
-import requests
 import logging
-
 import urllib.parse
+from time import strptime
+
+import pytz
+import requests
 
 lo = logging.getLogger("jira")
+TIMESTAMP_FORMAT = r"%Y-%m-%dT%H:%M:%S.%f%z"
 
-class Jira():
+
+class Jira:
     def __init__(self, config) -> None:
-        #config = jiraDetails()
+        # config = jiraDetails()
         self.valid = config.valid
         self.host = config.host
         self.name = config.name
         self.token = config.key
 
-
     def fetchJiraTickets(self, jql) -> dict:
         "takes a JQL string, encodes it, send its to Jira, and returns a dict of tickets, with the ticket ID (PRJ-123) as the dict key"
-        headers = {"Content-Type" : "application/json", "Authorization" : "Basic %s" % self.token}
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Basic %s" % self.token,
+        }
         jql = urllib.parse.quote(jql)
-        
+
         url = f"https://{self.host}/rest/api/2/search?jql={jql}&fields=key,summary,description,status,priority,assignee"
         r = requests.get(url, headers=headers)
         if r.status_code != 200:
-            print("ERROR: %s could not fetch Jira tickets\n%s" % (r.status_code, r.content))
+            print(
+                "ERROR: %s could not fetch Jira tickets\n%s"
+                % (r.status_code, r.content)
+            )
             return []
         loadedTickets = json.loads(r.content)
         incomingTickets = {}
@@ -31,15 +41,27 @@ class Jira():
 
             new_ticket = jiraTicket()
             new_ticket.from_dict(ticket)
-            
-            incomingTickets[ticket["key"]] = new_ticket
-            #print(json.dumps(ticket,indent=2))
-        return incomingTickets
-        
 
-class jiraTicket():
+            incomingTickets[ticket["key"]] = new_ticket
+            # print(json.dumps(ticket,indent=2))
+        return incomingTickets
+
+
+class jiraTicket:
     "represents a single Jira ticket"
-    def __init__(self, key = "", summary = "", assignee_id = "", assignee_name = "", status = "", priority = "", description = "") -> None:
+
+    def __init__(
+        self,
+        key="",
+        summary="",
+        assignee_id="",
+        assignee_name="",
+        status="",
+        priority="",
+        description="",
+        created=datetime.datetime.min,
+        updated=datetime.datetime.min,
+    ) -> None:
         self.key = key
         self.summary = summary
         self.description = description
@@ -47,9 +69,11 @@ class jiraTicket():
         self.assignee_name = assignee_name
         self.status = status
         self.priority = priority
+        self.created = created
+        self.updated = updated
         pass
 
-    def from_dict(self,new_dict:dict):
+    def from_dict(self, new_dict: dict):
         fields = new_dict["fields"] if "fields" in new_dict else {}
         assignee_dict = fields["assignee"] if "assignee" in fields else {}
         assignee_dict = {} if assignee_dict is None else assignee_dict
@@ -57,61 +81,95 @@ class jiraTicket():
 
         self.key = new_dict["key"] if "key" in new_dict else self.key
         self.summary = fields["summary"] if "summary" in fields else self.summary
-        self.description = fields["description"] if "description" in fields else self.description
-        self.assignee_id = assignee_dict["key"] if "key" in assignee_dict else self.assignee_id
-        self.assignee_id = assignee_dict["accountId"] if "accountId" in assignee_dict else self.assignee_id
-        self.assignee_name = assignee_dict["displayName"] if "displayName" in assignee_dict else self.assignee_name
-        self.priority = priority_dict["name"] if "name" in priority_dict else self.priority
+        self.description = (
+            fields["description"] if "description" in fields else self.description
+        )
+        self.assignee_id = (
+            assignee_dict["key"] if "key" in assignee_dict else self.assignee_id
+        )
+        self.assignee_id = (
+            assignee_dict["accountId"]
+            if "accountId" in assignee_dict
+            else self.assignee_id
+        )
+        self.assignee_name = (
+            assignee_dict["displayName"]
+            if "displayName" in assignee_dict
+            else self.assignee_name
+        )
+        self.priority = (
+            priority_dict["name"] if "name" in priority_dict else self.priority
+        )
         self.status = new_dict["status"] if "status" in new_dict else self.status
-        pass 
 
-class jiraDetails():
+        self.created = (
+            strptime(fields["created"], TIMESTAMP_FORMAT)
+            if "created" in fields
+            else self.created
+        )
+
+        self.updated = (
+            strptime(fields["updated"], TIMESTAMP_FORMAT)
+            if "updated" in fields
+            else self.updated
+        )
+        pass
+
+
+class jiraDetails:
     """represents the settings for a jira object"""
+
     def __init__(self) -> None:
         self.valid = False
         self.host = ""
         self.name = ""
         self.key = ""
-        self.primary_user_id = "" 
+        self.primary_user_id = ""
         self.issues_jql = ""
         self.trello_label = ""
-        self.trello_custom_field = "" 
+        self.trello_custom_field = ""
         self._lo = logging.getLogger("jiraDetails")
         pass
-    def from_dict(self, dict:dict):
+
+    def from_dict(self, dict: dict):
         self.name = dict["instanceName"] if "instanceName" in dict else ""
         self.host = dict["instanceHost"] if "instanceHost" in dict else ""
-        self.key = dict["bearerToken"] if "bearerToken" in dict else "" 
-        self.primary_user_id = dict["instanceUserID"] if "instanceUserID" in dict else ""
+        self.key = dict["bearerToken"] if "bearerToken" in dict else ""
+        self.primary_user_id = (
+            dict["instanceUserID"] if "instanceUserID" in dict else ""
+        )
         self.issues_jql = dict["instanceJQL"] if "instanceJQL" in dict else ""
-        self.trello_custom_field = dict["TrelloCustomFieldIDforJira"] if "TrelloCustomFieldIDforJira" in dict else ""
-        self.trello_label = dict["TrelloLabelForJiraCards"] if "TrelloLabelForJiraCards" in dict else ""
+        self.trello_custom_field = (
+            dict["TrelloCustomFieldIDforJira"]
+            if "TrelloCustomFieldIDforJira" in dict
+            else ""
+        )
+        self.trello_label = (
+            dict["TrelloLabelForJiraCards"] if "TrelloLabelForJiraCards" in dict else ""
+        )
         self.check_keys()
-        pass 
-
+        pass
 
     def check_keys(self):
         valid = True
         if self.name == "":
             lo.warning("jira obj name is blank")
-            valid = False 
+            valid = False
         if self.host == "":
             lo.warning("jira object host is blank")
-            valid = False 
+            valid = False
         if self.primary_user_id == "":
             lo.warning("jira object primary_user_id is blank")
-            valid = False 
+            valid = False
         if self.issues_jql == "":
             lo.warning("jira object issues_jql is blank")
-            valid = False 
+            valid = False
         if self.trello_custom_field == "":
             lo.warning("jira object trello_custom_field is blank")
-            valid = False 
+            valid = False
         if self.trello_label == "":
             lo.warning("jira object trello_label is blank")
-            valid = False 
+            valid = False
         if self.key == "":
             lo.warning("key missing from jira settings")
         self.valid = valid
-
-
