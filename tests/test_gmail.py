a02ccac16ef942ac9cb942d77d5666eea4adba1b
C'tri Goudie <CtriCoding@Ctri.co.uk>
2023-06-02 21:39:47 +0100

    test tweaks

--- tests/test_gmail.py
+++ tests/test_gmail.py
@@ -5,16 +5,17 @@ import pytest
 from serviceHelpers.gmail import make_new_token_from_refresh_bits, load_token, save_token 
 from serviceHelpers.gmail import extract_clientid_clientsecret_from_secretjson, InvalidCredentialsException, Gmail
 
-TEST_REFRESH_TOKEN = os.environ.get("GMAIL_REFRESH_TOKEN_TEST",None)
+TEST_REFRESH_TOKEN = os.environ.get("GMAIL_REFRESH_TOKEN",None)
 TEST_GMAIL_SECRET_JSON = os.environ.get("GMAIL_SECRET_JSON",None)
 TEST_EMAIL_ADDRESS = "github-tests@ctri.co.uk"
-@pytest.fixture
 
 
 def test_env_vars():
     assert TEST_REFRESH_TOKEN is not None
     assert TEST_GMAIL_SECRET_JSON is not None   
- 
+
+
+@pytest.fixture
 def gmail_service():
     secret_bits = extract_clientid_clientsecret_from_secretjson(TEST_GMAIL_SECRET_JSON)
     token = make_new_token_from_refresh_bits(TEST_REFRESH_TOKEN, secret_bits[0], secret_bits[1])
