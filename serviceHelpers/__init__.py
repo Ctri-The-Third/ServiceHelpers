027547dbee7089f486d0491f0c4b34c62da3fd88
C'tri Goudie <CtriCoding@Ctri.co.uk>
2025-10-01 13:03:35 +0100

    Bump version to 3.2.5 for release

--- serviceHelpers/__init__.py
+++ serviceHelpers/__init__.py
@@ -2,7 +2,7 @@ from serviceHelpers.models import ZendeskOrg, ZendeskTicket, ZendeskUser, Zendes
 from serviceHelpers.models import hueBulb
 from serviceHelpers.models import JiraTicket, JiraDetails
 
-__version__ = "3.2.2"
+__version__ = "3.2.5"
 
 from dotenv import load_dotenv
 load_dotenv()
